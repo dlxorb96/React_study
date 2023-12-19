1a34de5baa319df0fad3ebfb9cec1ec989ab554e
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2023-12-19 18:04:26 +0900

    primereact 사이드 바 개발중

--- src/App.js
+++ src/App.js
@@ -1,158 +1,22 @@
-//eslint-disable
+import {Button} from "primereact/button"
+import { useState } from "react";
 
-import { useState } from 'react';
-import './App.css';
-import React from 'react';
-import Donught from './Chart/Donught';
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-
+import { Menu } from 'primereact/menu';
+import { Toast } from 'primereact/toast';
+import Sidebar from "./Sidebar";
 function App() {
 
-  let header = "헤더"
-  // a는 useState에 담긴 값
-  // b는 useState를 수정하는 함수
-  // Destructuring문법
-  let num = [1,2];
-  let a = num[0];
-  let c = num[1];
-
-  // d는 1 f는 2가 들어감
-  let [d,f] = [1,2];
-
-  
-  let [title, changeTitle] = useState(["남자 코트 추천", "외투", "바지"]);
-  let [inputValue , setInputValue] = useState("");
-  
-  // useState에는 ['남자 코트 추천', 함수] 남음
-
-  let [likeCnt, changeLikeCnt] = useState([0,0,0]);
-  // 일반 변수는 갑자기 변경되면 html에 자동으로 반영안됨.
-  // 일반 변수는 .html() 이런식으로 넣어줘야 할듯
-  // state쓰던 html은 자동으로 재렌더링 됨.
-
-  const [modal, setModal] = useState(false)
-
-  const [selectedTItle, setSelectedTitle] = useState(0);
-
-  function clickLike(){
-    // state는 등호 변경x
-    likeCnt +=1
+  const style = {
+    width :"100px",
+    background : "red"
   }
 
-  return (
-    <div className="App">
-      <div className='header'>
-        <h4>탄소중립HUB샘플</h4>
-      </div>
-
-      {
-        title.map((e,i)=>{
-          return(
-            <div className='list' key={i}>
-              <h4 style={{display: 'inline-block'}}
-                onClick={()=>{
-                  setSelectedTitle(i);
-                  setModal(!modal);
-                }}>글 제목</h4><span onClick={()=>{
-                const arr = [...likeCnt]
-                arr[i] += 1
-                changeLikeCnt(arr)
-                }}>👍</span> {likeCnt[i]} 
-              <p>{e}</p>
-              <button onClick={()=>{
-                const copy = [...title]
-                copy.splice(i, 1);
-                changeTitle(copy)
-              }}>글 삭제</button>
-            </div>
-          )
-        })
-      }
-
-      {/* state변경함수 특징
-        기존 state == 신규 state의 경우 변경안해줌
-        =은 메모리 주소를 나타내는 거기때문에
-        기존 주소가 그대로이기 때문에
-        reference data type
-      */}
-      <button onClick={()=>{
-        let copy = [...title];
-        copy[0] = "여자코트추천";
-        changeTitle(copy)}}>
-        바꾸기
-      </button>
-
-      <button onClick={()=>{
-        modal === true ? setModal(false) : setModal(true)
-        }}>
-      모달토글
-      </button>
-
-      <button onClick={()=>{
-        let copy1 = [...title];
-        
-        
-        copy1.sort((a,b) => b.localeCompare(a));
-        console.log(copy1)
-        changeTitle(copy1)
-      }}>
-        가나다순정렬
-      </button>
-      <div>
-        <input onChange={(e)=>{
-          setInputValue(e.target.value)
-        }}></input>
-        <button onClick={()=>{
-          const copy = [...title];
-          copy.push(inputValue);
-          changeTitle(copy);
-        }}>
-          등록
-        </button>
-      </div>
-      
-      {
-        modal === true ? 
-          <Modal title={title} changeTitle = {changeTitle} selectedTItle = {selectedTItle} /> : null
-      }
-      <Modal2 />
-      <Donught width={"1000px"} height={"200px"}/>
-    </div>
-  );
-}
-
-function Modal(props){
-  return(
-    <div>
-      <div className='modal' style={{background : props.color}}>
-        <h4>{props.title[props.selectedTItle]}</h4>
-        <p>날짜</p>
-        <p>상세내용</p>
-        <button>글수정</button>
-      </div>
-    </div>
-  )
-}
-
-class Modal2 extends React.Component{
-  constructor(props){
-    super(props);
-    this.state ={
-      name : "kim",
-      age: 20
-
-    }
-  }
-  render(){
-    return(
-      <div>
-        <div>안녕 {this.state.age}{this.state.name}</div>
-        <button onClick={()=>{
-          this.setState({age:21})
-        }}>클릭</button>
-      </div>
+    return (
+        <Sidebar/>
     )
-  }
 }
 
 export default App;
--- src/Chart/Donught.js
+++ src/Chart/Donught.js
@@ -1,59 +1,111 @@
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
+// import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale,PointElement, BarElement, LineElement} from "chart.js";
+import { Chart as ChartJS } from 'chart.js/auto'
+import { Bar } from "react-chartjs-2";
 import { useState } from 'react';
+import ChartDataLables from "chartjs-plugin-datalabels";
 
-ChartJS.register(ArcElement, Tooltip, Legend);
 
 
-function Donught(props){
-    let [chatData, setChatData] = useState([]);
-    let option = {
-        // responsive : true,  // 반응형 여부
-        plugin : {
-            legend : {
-                position : "top"
-            },
-            title : {
-                display : true,
-                text : "도넛차트 테스트"
-            }
+
+// ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement, PointElement, LineElement);
+
+
+function Donught(props) {
+  let [chatData, setChatData] = useState([]);
+  let option = {
+    // responsive : true,  // 반응형 여부
+    plugin: {
+      legend: {
+        position: "top"
+      },
+      title: {
+        display: true,
+        text: "도넛차트 테스트"
+      }
+    },
+    plugins: {
+      color: "#fff", // color of the label text
+      datalabels : {
+        formatter: function (value, context) {
+          // Grab the label for this value
+          console.log(1);
+          const label = context.dataset.data[context.dataIndex];
+  
+          // Format the number with 2 decimal places
+  
+          const formattedVal = Intl.NumberFormat("en-US", {
+            minimumFractionDigits: 20,
+          }).format(value);
+  
+          return `${label}`;
         }
+      }
+      
     }
-    let labels = [1,2,3,4];
-    const data = {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-          },
+  }
+  let labels = [1, 2, 3, 4];
+  const data = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        type: "bar",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
         ],
-      };
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+      {
+        label: '# of Votes',
+        type: "line",
+        data: [10, 9, 8, 7, 6, 5],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      }
+    ],
+  };
 
-    return(
-        <div style={{width : props.width, height : props.height}}>
-            도넛차트임
-            <Doughnut data={data} />
+  return (
+    <div style={{ width: props.width, height: props.height }}>
+      도넛차트임
+      {/* ChartDataLables 라이브러리 사용
+        plugins={[ChartDataLables]} porps로 전달 후
+        option.plugins로 설정
+      */}
+      <Bar data={data} plugins={[ChartDataLables]} options={option} />
 
-        </div>
-    )
+    </div>
+    
+  )
 }
 
 export default Donught;
